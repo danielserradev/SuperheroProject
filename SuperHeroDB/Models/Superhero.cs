b36950414554867c6ba45727391e95f04cfd9477
danny serra <danielserradev@gmail.com>
2019-10-24 14:13:11 -0500

    finished the tutorial for the 3rd time. now no problems

---
+++ SuperHeroDB/Models/Superhero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroDB.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string alterEgo { get; set; }
+        public string primarySuperheroAbility { get; set; }
+        public string secondarySuperheroAbility { get; set; }
+        public string catchphrase { get; set; }
+
+
+    }
+}
