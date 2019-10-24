a08a3400100f3f867abea28ab452f5e412bfc0b0
danny serra <danielserradev@gmail.com>
2019-10-24 13:21:01 -0500

    init commit

---
+++ SuperHeroDB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperHeroDB.Startup))]
+namespace SuperHeroDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
